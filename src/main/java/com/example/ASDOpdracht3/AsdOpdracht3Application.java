ecd968705f7011f8482ffdb8ccde99c4111ad94c
DAdeRijk <damian04@msn.com>
2020-12-04 15:27:29 +0100

    initial

---
+++ src/main/java/com/example/ASDOpdracht3/AsdOpdracht3Application.java
@@ -0,0 +1,13 @@
+package com.example.ASDOpdracht3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsdOpdracht3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsdOpdracht3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ASDOpdracht3/AsdOpdracht3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ASDOpdracht3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsdOpdracht3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
