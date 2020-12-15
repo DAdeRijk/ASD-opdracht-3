91c19860815974ba5a724c8db1b924c51774f5fa
Tim <23260223+timhoogeland@users.noreply.github.com>
2020-12-15 20:31:53 +0100

    fix path + empty test class

---
+++ src/test/java/com/example/ASDOpdracht3/customer/registerCustomerTest.java
@@ -0,0 +1,13 @@
+package com.example.ASDOpdracht3.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class registerCustomerTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
