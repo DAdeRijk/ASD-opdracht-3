44a9b1d293305cfc490406f90e4eed89ab7f800f
ehoudijk <43175355+ehoudijk@users.noreply.github.com>
2020-12-15 21:20:23 +0100

    controller createTCM update

--- src/main/java/com/example/ASDOpdracht3/TrainManagement/controller/TrainCompositionMessage/TrainCompositionMessageController.java
+++ src/main/java/com/example/ASDOpdracht3/TrainManagement/controller/TrainCompositionMessage/TrainCompositionMessageController.java
@@ -1,13 +1,21 @@
 package com.example.ASDOpdracht3.TrainManagement.controller.TrainCompositionMessage;
 
+import com.example.ASDOpdracht3.TrainManagement.application.TrainCompositionMessage.TrainCompositionMessageImpl;
+import com.example.ASDOpdracht3.TrainManagement.application.TrainCompositionMessage.TrainCompositionMessageInterface;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
 
 @RestController
 public class TrainCompositionMessageController {
-    @GetMapping("/create-traincompositionmessage")
-    public String createTrainCompositionMessage() {
-        return String.format("Create train API");
+    @PutMapping("/create-traincompositionmessage")
+    public ResponseEntity<String> createTrainCompositionMessage(int id, String messageStatus, String sender, String messageType, LocalDateTime messageDateTime, LocalDateTime startDate, String trainCompositionId) {
+        TrainCompositionMessageInterface trainCompositionMessageImpl = new TrainCompositionMessageImpl();
+        return new ResponseEntity<>(trainCompositionMessageImpl.createTrainCompositionMessage(id, messageStatus, sender, messageType, messageDateTime, startDate, trainCompositionId), HttpStatus.OK);
     }
 
     @GetMapping("/delete-traincompositionmessage")
