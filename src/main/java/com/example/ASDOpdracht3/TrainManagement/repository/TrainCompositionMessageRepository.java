328fa75c02396fc4109256897a4c6835d8c99ce8
ehoudijk <43175355+ehoudijk@users.noreply.github.com>
2020-12-15 20:34:39 +0100

    findbyid string naar int

--- src/main/java/com/example/ASDOpdracht3/TrainManagement/repository/TrainCompositionMessageRepository.java
+++ src/main/java/com/example/ASDOpdracht3/TrainManagement/repository/TrainCompositionMessageRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface TrainCompositionMessageRepository extends JpaRepository<TrainCompositionMessage, String> {
-    Optional<TrainCompositionMessage> findById(String id);
+    Optional<TrainCompositionMessage> findById(int id);
 }
