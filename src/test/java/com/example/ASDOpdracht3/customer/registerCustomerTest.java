dbf57597e6e65b7f83e73e60a7164bc3f7b667ee
Tim <23260223+timhoogeland@users.noreply.github.com>
2020-12-15 21:29:25 +0100

    tests

--- src/test/java/com/example/ASDOpdracht3/customer/registerCustomerTest.java
+++ src/test/java/com/example/ASDOpdracht3/customer/registerCustomerTest.java
@@ -1,13 +1,44 @@
 package com.example.ASDOpdracht3.customer;
 
+import com.example.ASDOpdracht3.CustomerManagement.application.Customer.CustomerImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class registerCustomerTest {
+    @MockBean
+    private CustomerImpl customerImpl;
 
     @Test
-    void contextLoads() {
+    public void invalidCompanyShouldReturnInvalid() {
+        assertThat(customerImpl.registerCustomer("NL45RABO0118343319", "Henk", "Petersen", "goodemail@mymail.com", 12).equals("COMPANY_NOT_LISTED"));
+    }
+    @Test
+    public void validCompanyShouldNotReturnInvalid() {
+        assertThat(!customerImpl.registerCustomer("NL45RABO0118343319","Henk","Petersen","goodemail@mymail.com",4875).equals("COMPANY_NOT_LISTED"));
+    }
+    @Test
+    public void invalidEmailShouldReturnInvalid() {
+        assertThat(customerImpl.registerCustomer("NL45RABO0118343319","Henk","Petersen","bademail",4875).equals("INVALID_EMAIL"));
+    }
+    @Test
+    public void validEmailShouldNotReturnInvalid() {
+        assertThat(!customerImpl.registerCustomer("NL45RABO0118343319","Henk","Petersen","goodemail@mymail.com",4875).equals("INVALID_EMAIL"));
+    }
+    @Test
+    public void invalidIbanShouldReturnInvalid() {
+        assertThat(customerImpl.registerCustomer("NL45RABORABO01183433","Henk","Petersen","goodemail@mymail.com",4875).equals("INVALID_IBAN"));
+    }
+    @Test
+    public void validIbanShouldNotReturnInvalid() {
+        assertThat(!customerImpl.registerCustomer("NL45RABO0118343319","Henk","Petersen","goodemail@mymail.com",4875).equals("INVALID_IBAN"));
+    }
+    @Test
+    public void validInfoShouldReturnSuccess() {
+        assertThat(customerImpl.registerCustomer("NL45RABO0118343319","Henk","Petersen","goodemail@mymail.com",4875).equals("SUCCESS"));
     }
 
 }
