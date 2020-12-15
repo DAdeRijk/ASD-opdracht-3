d34290e05b47171f321bfced56ac17f1eda07cc2
DAdeRijk <damian04@msn.com>
2020-12-15 22:02:08 +0100

    cancelTrainCompositionMessageTest

---
+++ src/test/java/com/example/ASDOpdracht3/trainCompositionMessage/cancelTrainCompositionMessageTest.java
@@ -0,0 +1,24 @@
+package com.example.ASDOpdracht3.trainCompositionMessage;
+
+
+import com.example.ASDOpdracht3.TrainManagement.application.TrainCompositionMessage.TrainCompositionMessageImpl;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class cancelTrainCompositionMessageTest {
+    @MockBean
+    private TrainCompositionMessageImpl trainCompositionMessageImpl;
+
+    @Test
+    public void invalidTrainCompositionMessageIdShouldReturnInvalid() {
+        assertThat(trainCompositionMessageImpl.cancelTrainCompositionMessage(0).equals("Error"));
+    }
+    @Test
+    public void validIdShouldReturnSuccess() {
+        assertThat(trainCompositionMessageImpl.cancelTrainCompositionMessage(12).equals("Success"));
+    }
+}
