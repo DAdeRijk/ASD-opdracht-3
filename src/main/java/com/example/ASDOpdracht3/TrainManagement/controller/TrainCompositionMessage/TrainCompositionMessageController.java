fe946055f1a3d326d15eebbb149b942daffc5253
ehoudijk <43175355+ehoudijk@users.noreply.github.com>
2020-12-15 21:28:30 +0100

    Update TrainCompositionMessageController.java

--- src/main/java/com/example/ASDOpdracht3/TrainManagement/controller/TrainCompositionMessage/TrainCompositionMessageController.java
+++ src/main/java/com/example/ASDOpdracht3/TrainManagement/controller/TrainCompositionMessage/TrainCompositionMessageController.java
@@ -4,23 +4,21 @@ import com.example.ASDOpdracht3.TrainManagement.application.TrainCompositionMess
 import com.example.ASDOpdracht3.TrainManagement.application.TrainCompositionMessage.TrainCompositionMessageInterface;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 
 @RestController
 public class TrainCompositionMessageController {
-    @PutMapping("/create-traincompositionmessage")
-    public ResponseEntity<String> createTrainCompositionMessage(int id, String messageStatus, String sender, String messageType, LocalDateTime messageDateTime, LocalDateTime startDate, String trainCompositionId) {
-        TrainCompositionMessageInterface trainCompositionMessageImpl = new TrainCompositionMessageImpl();
-        return new ResponseEntity<>(trainCompositionMessageImpl.createTrainCompositionMessage(id, messageStatus, sender, messageType, messageDateTime, startDate, trainCompositionId), HttpStatus.OK);
     TrainCompositionMessageInterface trainCompositionMessageApplication = new TrainCompositionMessageImpl();
 
-    @PatchMapping("/cancel-traincompositionmessage")
+    @PutMapping("/create-traincompositionmessage")
+    public ResponseEntity<String> createTrainCompositionMessage(int id, String messageStatus, String sender, String messageType, LocalDateTime messageDateTime, LocalDateTime startDate, String trainCompositionId) {
+        return new ResponseEntity<>(trainCompositionMessageApplication.createTrainCompositionMessage(id, messageStatus, sender, messageType, messageDateTime, startDate, trainCompositionId), HttpStatus.OK);
+    }
+
+    @GetMapping("/cancel-traincompositionmessage")
     public String cancelTrainCompositionMessage(@RequestParam int id) {
         String returnValue = trainCompositionMessageApplication.cancelTrainCompositionMessage(id);
         return returnValue;
