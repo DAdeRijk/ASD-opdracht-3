9633979b8f64dd51119a221d8b560b33e1b237bb
Cor de Kruijf <cor.dekruijf@student.hu.nl>
2020-12-15 21:34:35 +0100

    Create TrainCompositionTest.java

---
+++ src/test/java/com/example/ASDOpdracht3/TrainManagement/application/TrainComposition/TrainCompositionTest.java
@@ -0,0 +1,16 @@
+package com.example.ASDOpdracht3.TrainManagement.application.TrainComposition;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TrainCompositionTest {
+
+    @Test
+    void createTrainComposition() {
+    }
+
+    @Test
+    void findById() {
+    }
+}
